5841aebc2df2cab26c25593f4333c6925675f41b
andrew-zhuang <52724966+andrew-zhuang@users.noreply.github.com>
2020-05-05 21:02:33 +0800

    Update lab03.py

--- lab03/lab03.py
+++ lab03/lab03.py
@@ -13,8 +13,9 @@ class VariationalCalculus:
         """
         self.left = left
         self.right = right
-        self.__points_number = points_list
+        self.__point_list = points_list
         self.function = function
+        self.__points_number = None
         self.points = None
         self.values = None
         self.Trapezoid = None
@@ -24,10 +25,14 @@ class VariationalCalculus:
         self.eps_index_Trapezoid = None
         self.eps_index_Simpson = None
 
+    def __init_points(self):
+        self.__points_number = [pow(2, i) for i in self.__point_list]
+        for i in range(1, len(self.__point_list)+1):
+            self.__points_number.append(i * self.__points_number[i - 1])
+
     def __convert_points(self):
         def calculate(number):
-            number = pow(2, number)
-
+            # print(number)
             return list(np.linspace(self.left, self.right, int(number+1)))
         self.points = list(map(calculate, iter(self.__points_number)))
 
@@ -37,23 +42,24 @@ class VariationalCalculus:
 
     def __Trapezoid_method(self):
         def calculate(value_list):
-            length = len(value_list)
+            length = len(value_list) - 1
             h = (self.right - self.left) / float(length)
-            return h / 2 * (2 * sum(value_list) - value_list[0] - value_list[length - 1])
+            return h / 2 * (2 * sum(value_list) - value_list[0] - value_list[length])
 
-        self.Trapezoid = [calculate(value_list) for value_list in self.points]
+        self.Trapezoid = [calculate(value_list) for value_list in self.values]
 
     def __Simpson_method(self):
         def calculate(value_list):
-            length = len(value_list)
+            length = len(value_list) - 1
             h = (self.right - self.left) / float(length)
             odds_list = value_list[1::2]
             even_list = value_list[::2]
-            return h / 3 * (2 * sum(even_list) + 4 * sum(odds_list) - value_list[0] - value_list[length - 1])
-        self.Simpson = [calculate(value_list) for value_list in self.points]
+            return h / 3 * (2 * sum(even_list) + 4 * sum(odds_list) - value_list[0] - value_list[length])
+        self.Simpson = [calculate(value_list) for value_list in self.values]
 
     def __eps_Trapezoid(self):
         integral_result = integrate.quad(self.function, self.left, self.right)
+        # print(integral_result)
         self.eps_Trapezoid = list(map(lambda s: integral_result[0] - s, iter(self.Trapezoid)))
 
     def __eps_Simpson(self):
@@ -62,19 +68,22 @@ class VariationalCalculus:
 
     def __eps_Trapezoid_index(self):
         length = len(self.eps_Trapezoid)
+        real_length = int(length / 2)
         self.eps_index_Trapezoid = []
-        for i in range(length - 1):
-            res = self.eps_Trapezoid[i] / self.eps_Trapezoid[i+1] / np.log(2)
+        for i in range(real_length):
+            res = np.log(np.abs(self.eps_Trapezoid[i] / self.eps_Trapezoid[i + real_length])) / (np.log(i + 1))
             self.eps_index_Trapezoid.append(res)
 
     def __eps_Simpson_index(self):
         length = len(self.eps_Simpson)
+        real_length = int(length / 2)
         self.eps_index_Simpson = []
-        for i in range(length - 1):
-            res = (self.eps_Simpson[i] / self.eps_Simpson[i+1]) / np.log(2)
+        for i in range(real_length):
+            res = np.log(np.abs(self.eps_Simpson[i] / self.eps_Simpson[i + real_length])) / (np.log(i + 1))
             self.eps_index_Simpson.append(res)
 
     def solve(self):
+        self.__init_points()
         self.__convert_points()
         self.__function()
         self.__Trapezoid_method()
@@ -90,9 +99,9 @@ if __name__ == '__main__':
     variational = VariationalCalculus(number_list, 1, 5, np.sin)
     variational.solve()
     print("复化梯形积分公式的误差和误差阶为：")
-    for i in range(len(number_list) - 1):
+    for i in range(len(number_list)):
         print(i, variational.eps_Trapezoid[i], variational.eps_index_Trapezoid[i])
     print("复化Simpson积分公式的误差和误差阶为：")
-    for i in range(len(number_list) - 1):
+    for i in range(len(number_list)):
         print(i, variational.eps_Simpson[i], variational.eps_index_Simpson[i])
     print("done\n")
